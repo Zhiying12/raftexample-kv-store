fd2c597a2acbea8639acbe7407240664faa0ab55
Zhiying Liang <zvl5490@psu.edu>
2023-08-24 11:19:02 -0400

    add default cluster addresses

--- main.go
+++ main.go
@@ -23,7 +23,9 @@ import (
 )
 
 func main() {
-	cluster := flag.String("cluster", "http://127.0.0.1:9021", "comma separated cluster peers")
+	cluster := flag.String("cluster", "http://172.31.2.227:10000,"+
+		"http://172.31.8.130:11000,http://172.31.1.13:12000",
+		"comma separated cluster peers")
 	id := flag.Int("id", 1, "node ID")
 	kvport := flag.Int("port", 9121, "key-value server port")
 	join := flag.Bool("join", false, "join an existing cluster")
